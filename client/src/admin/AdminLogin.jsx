07ff04380d7d1951f7e11f3d32f566cdcb28e060
startraders9999 <startraders99@proton.me>
2025-08-12 13:52:22 +0530

    Update AdminLogin.jsx

--- client/src/admin/AdminLogin.jsx
+++ client/src/admin/AdminLogin.jsx
@@ -10,7 +10,7 @@ function AdminLogin() {
     e.preventDefault();
 
     // ✅ Hardcoded admin credentials check
-    if (username === 'admin' && password === 'admin') {
+    if (username === 'starking' && password === 'star@lokesh') {
       // ✅ Save login token in localStorage
       localStorage.setItem('admin-auth', 'true');
 
